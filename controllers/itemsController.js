d0fc7c538d75a71d43e9f4134c85631c23fcd58e
rouxxi <laskar.gwenvael@gmail.com>
2021-02-03 19:31:52 +0100

    add route post on items

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -10,6 +10,17 @@ itemsController.findAll = (req, res) => {
             res.status(200).send(result)
         }
     })
-}
+};
+
+itemsController.createItem = (req, res) => {
+    const { label, description, linkImage, price, user } = req.body;
+    connection.query('INSERT into item SET label = ?,  price = ?, description = ? , picture = ? , creator = ?', [label, price, description, linkImage, user], (error, result) => {
+        if (error) {
+            res.status(500).send(error)
+        } else {
+            res.status(200).send(result)
+        }
+    })
+};
 
 module.exports = itemsController;
--- router/items/index.js
+++ router/items/index.js
@@ -1,6 +1,11 @@
-const router = require('express').Router();
-const itemssController = require('../../controllers/itemsController')
+const express = require('express');
+const router = require('express').Router({ mergeParams: true });
+const itemsController = require('../../controllers/itemsController')
 
-router.get('/', itemssController.findAll)
+router.use(express.json());
+
+router.get('/', itemsController.findAll);
+router.post('/', itemsController.createItem);
+
 
 module.exports = router;
