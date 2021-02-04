e15d1d0a5635c489043ef86d975ad7bc6b9bd5b3
rouxxi <laskar.gwenvael@gmail.com>
2021-02-04 11:40:22 +0100

    put hidden on get all user / add email and password verification / creation token with jwt

--- middleWare/auth/authUser.js
+++ middleWare/auth/authUser.js
@@ -1,44 +1,10 @@
-const connection = require('../service/mysql/connection');
+const connection = require('../../service/mysql/connection');
 const bcrypt = require('bcrypt');
-const usersController = {};
 var jwt = require('jsonwebtoken');
+const verification = {};
 
-usersController.findAll = (req, res) => {
-    connection.query('SELECT * from user', (error, result) => {
-        if (error) {
-            res.status(500).send(error)
-        } else {
-            res.status(200).send(result.map((user) => {
-                return {...user, password: 'hidden' };
-            }))
-        }
-    })
-}
 
-usersController.createUser = (req, res) => {
-    const { firstName, lastName, nickName, email, password } = req.body;
-
-    bcrypt.hash(password, 10, function(err, hash) {
-        connection.query('INSERT into user SET firstName = ?,  lastName = ?, nickName = ? , email = ? , password = ? ', [firstName, lastName, nickName, email, hash], (error, result) => {
-            if (error) {
-                res.status(500).send(error)
-            } else {
-                res.status(200).send({
-
-                    result: {
-                        ...result,
-                        password: 'hidden',
-                        message: 'Le compte à bien été crée !'
-                    },
-
-                })
-            }
-        })
-    });
-
-};
-
-usersController.login = (req, res) => {
+verification.email = (req, res) => {
     const { email, password } = req.body;
 
     connection.query('SELECT * from user WHERE email = ?', [email], (error, result) => {
@@ -68,4 +34,12 @@ usersController.login = (req, res) => {
     });
 }
 
-module.exports = usersController;
+
+// verification.token = (req, res) => {
+//     const { userId } = req.body;
+//     jwt.sign({ foo: }, process.env.JWT, { algorithm: 'RS256' }, function(err, token) {
+//         console.log(token);
+//     })
+// };
+
+module.exports = verification;
--- router/users/index.js
+++ router/users/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const usersController = require('../../controllers/usersController')
-
 router.get('/', usersController.findAll);
-router.post('/', usersController.createUser);
 
+router.post('/register', usersController.createUser);
+router.post('/login', usersController.login);
 module.exports = router;
